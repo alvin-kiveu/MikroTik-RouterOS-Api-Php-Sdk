1dee445f23049940e282c097673bb7eec380a2e5
Alvin Kiveu <67068458+alvin-kiveu@users.noreply.github.com>
2024-04-18 12:00:00 +0300

    anew update

--- src/Pool.php
+++ src/Pool.php
@@ -7,7 +7,7 @@ use MikrotikSdk\MikrotikSdk;
 
 class Pool extends MikrotikSdk
 {
-  
+
 
     public function getPool($ip, $login, $password)
     {
@@ -15,6 +15,23 @@ class Pool extends MikrotikSdk
        if ($connect) {
           // Get the pool
           $this->api->write('/ip/pool/print');
+          $pool = $this->api->read();
+          $this->disconnect();
+          return json_encode($pool);
+       }else{
+            return json_encode(['error' => 'Failed to connect to the Mikrotik']);
+       }
+    }
+
+
+    public function addPool($ip, $login, $password, $name, $ranges)
+    {
+       $connect = $this->connect($ip, $login, $password);
+       if ($connect) {
+          // Add the pool
+          $this->api->write('/ip/pool/add', false);
+          $this->api->write('=name='.$name, false);
+          $this->api->write('=ranges='.$ranges);
           $pool = $this->api->read();
           $this->disconnect();
           return json_encode($pool);
