dd424bf1d49621b74d75c58f7b8eeb6840be14f4
Alvin Kiveu <67068458+alvin-kiveu@users.noreply.github.com>
2024-04-18 13:04:20 +0300

    2.4.2 update

--- src/Pool.php
+++ src/Pool.php
@@ -39,4 +39,39 @@ class Pool extends MikrotikSdk
             return json_encode(['error' => 'Failed to connect to the Mikrotik']);
        }
     }
+
+
+    public function removePool($ip, $login, $password, $id)
+    {
+       $connect = $this->connect($ip, $login, $password);
+       if ($connect) {
+          // Remove the pool
+          $this->api->write('/ip/pool/remove', false);
+          $this->api->write('=.id='.$id);
+          $pool = $this->api->read();
+          $this->disconnect();
+          return json_encode($pool);
+       }else{
+            return json_encode(['error' => 'Failed to connect to the Mikrotik']);
+       }
+    }
+
+    public function editPool($ip, $login, $password, $id, $name, $ranges)
+    {
+       $connect = $this->connect($ip, $login, $password);
+       if ($connect) {
+          // Edit the pool
+          $this->api->write('/ip/pool/set', false);
+          $this->api->write('=.id='.$id, false);
+          $this->api->write('=name='.$name, false);
+          $this->api->write('=ranges='.$ranges);
+          $pool = $this->api->read();
+          $this->disconnect();
+          return json_encode($pool);
+       }else{
+            return json_encode(['error' => 'Failed to connect to the Mikrotik']);
+       }
+    }
+
+    
 }
