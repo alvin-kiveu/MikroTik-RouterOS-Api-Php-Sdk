c8302a28d4fce3e161f748226c5e901731e28841
Alvin Kiveu <67068458+alvin-kiveu@users.noreply.github.com>
2024-04-18 11:55:02 +0300

    adding new data

--- src/MikrotikSdk.php
+++ src/MikrotikSdk.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace MikrotikSdk;
 
 include_once 'RouterosAPI.php';
@@ -7,41 +8,21 @@ use RouterosAPI;
 
 class MikrotikSdk
 {
-  private $api;
+    protected $api;
 
-  public function __construct()
-  {
-      $this->api = new RouterosAPI();
-  }
+    public function __construct()
+    {
+        $this->api = new RouterosAPI();
+    }
 
-  public function connect($ip, $login, $password)
-  {
-      return $this->api->connect($ip, $login, $password);
-  }
+    public function connect($ip, $login, $password)
+    {
+        return $this->api->connect($ip, $login, $password);
+    }
 
-  public function disconnect()
-  {
-      $this->api->disconnect();
-  }
+    public function disconnect()
+    {
+        $this->api->disconnect();
+    }
 
-  public function executeCommand($command, $param2 = true)
-  {
-      return $this->api->write($command, $param2);
-  }
-
-  public function sendCommand($com, $arr = array())
-  {
-      return $this->api->comm($com, $arr);
-  }
-
-  public function parseResponse($response)
-  {
-      return $this->api->parseResponse($response);
-  }
-
-
-  public function __destruct()
-  {
-      $this->disconnect();
-  }
 }
---
+++ src/Pool.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace MikrotikSdk;
+
+use MikrotikSdk\MikrotikSdk;
+
+
+class Pool extends MikrotikSdk
+{
+
+    public function getPool($ip, $login, $password)
+    {
+       $connect = $this->connect($ip, $login, $password);
+       if ($connect) {
+          // Get the pool
+          $this->api->write('/ip/pool/print');
+          $pool = $this->api->read();
+          $this->disconnect();
+          return json_encode($pool);
+       }else{
+            return json_encode(['error' => 'Failed to connect to the Mikrotik']);
+       }
+    }
+}
